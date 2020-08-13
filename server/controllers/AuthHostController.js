09ec3f62764e8ee7a60fe02b9b253da2c8d23507
tim hopp <timothyjhopp@gmail.com>
2020-08-13 11:18:54 -0600

    apiSoCLOSE

--- server/controllers/AuthHostController.js
+++ server/controllers/AuthHostController.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import BaseController from '../utils/BaseController';
-// import { AuthHostService } from '../services/AuthHostService.js'
+import { authHostService } from '../services/AuthHostService.js'
 
 const SpotifyWebApi = require('spotify-web-api-node');
 let scopes = ['user-read-private', 'user-read-email', 'playlist-modify-public', 'playlist-modify-private']
@@ -9,26 +9,46 @@ let scopes = ['user-read-private', 'user-read-email', 'playlist-modify-public', 
 const spotifyApi = new SpotifyWebApi({
   clientId: process.env.SPOTIFY_CLIENT_ID,
   clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
-  redirectUri: 'http://localhost:8080',
+  redirectUri: 'http://localhost:3000/callback',
 });
-
 export class AuthHostController extends BaseController {
 
   constructor() {
-    super("authorize")
+    super("")
     console.log('AuthHostController active');
     this.router
-      .get('', this.authorizeHost)
+      .get('/login', this.authorizeHost)
+      .get('/callback', this.authCallBack)
   }
 
 
   async authorizeHost(req, res, next) {
     try {
       let html = await spotifyApi.createAuthorizeURL(scopes, '')
-      console.log(html);
+      // let consoleHtml = new URL(html);
       res.redirect(html)
+      
       // let data = await AuthHostService.authorizeHost(spotifyApi)
     } catch (error) { next(error) }
+
+
+  }
+  async authCallBack(req, res, next) {
+    const { code } = req.query;
+    console.log(code)
+    try {
+      let data = await spotifyApi.authorizationCodeGrant(code)
+      const { access_token, refresh_token, expires_in } = data.body;
+      console.log(data.body);
+
+      spotifyApi.setAccessToken(access_token);
+      spotifyApi.setRefreshToken(refresh_token);
+      authHostService.setHostTokens(access_token, refresh_token, expires_in)
+      res.redirect('http://localhost:8080');
+    } catch(error){
+      res.redirect('/#/error/invalid token')
+    }
+
 
 
   }
--- server/services/AuthHostService.js
+++ server/services/AuthHostService.js
@@ -1,48 +1,18 @@
-// import { dbContext } from "../db/DbContext"
-// import { BadRequest } from "../utils/Errors"
+import { dbContext } from "../db/DbContext";
+import { BadRequest } from "../utils/Errors";
 
+class AuthHostService {
+  async setHostTokens(access_token, refresh_token, expires_in) {
+    let HostTokens = await dbContext.HostTokens.create(access_token, refresh_token, expires_in);
+    return HostTokens;
+  }
+  async findById(id) {
+    let value = await dbContext.Search.findById(id);
+    if (!value) {
+      throw new BadRequest("Invalid Id");
+    }
+    return value;
+  }
+}
 
-// class AuthHostService { 
-
-//   async authorizeHost(spotifyApiData){
-//     let res = await 
-//   }
-// }
-
-// const SpotifyWebApi = require('spotify-web-api-node');
-// let scopes = ['user-read-private', 'user-read-email', 'playlist-modify-public', 'playlist-modify-private']
-
-// require('dotenv').config();
-
-// const spotifyApi = new SpotifyWebApi({
-//   clientId: process.env.SPOTIFY_CLIENT_ID,
-//   clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
-//   redirectUri: 'http://localhost:8080',
-// });
-
-// router.get('/', function (req, res, next) {
-//   res.render('index', { title: 'Express' });
-// });
-
-// router.get('/login', (req, res) => {
-//   var html = spotifyApi.createAuthorizeURL(scopes)
-//   console.log(html)
-//   res.send(html + "&show_dialog=true")
-// })
-
-// router.get('/callback', async (req, res) => {
-//   const { code } = req.query;
-//   console.log(code)
-//   try {
-//     var data = await spotifyApi.authorizationCodeGrant(code)
-//     const { access_token, refresh_token } = data.body;
-//     spotifyApi.setAccessToken(access_token);
-//     spotifyApi.setRefreshToken(refresh_token);
-
-//     res.redirect('http://localhost:3001/home');
-//   } catch (err) {
-//     res.redirect('/#/error/invalid token');
-//   }
-// });
-
-// export const AuthHostService = new AuthHostService()
+export const authHostService = new AuthHostService();
