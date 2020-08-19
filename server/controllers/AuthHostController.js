56f07cbaa1338e2693d60d1bc0547cd21a0aaf8c
MickShannahan <beasticorn@gmail.com>
2020-08-19 09:54:51 -0600

    changed the auth host to grants

--- server/controllers/AuthHostController.js
+++ server/controllers/AuthHostController.js
@@ -1,31 +1,26 @@
 import express from 'express';
 import BaseController from '../utils/BaseController';
 import { authHostService } from '../services/AuthHostService.js'
-
+import auth0provider from "@bcwdev/auth0provider";
 const SpotifyWebApi = require('spotify-web-api-node');
 let scopes = ['streaming', 'user-read-private', 'user-read-email', 'playlist-modify-public', 'playlist-modify-private']
-
-
 const spotifyApi = new SpotifyWebApi({
   clientId: process.env.SPOTIFY_CLIENT_ID,
   clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
   redirectUri: 'http://localhost:3000/callback',
 });
-
 let unsafe = {}
-
 export class AuthHostController extends BaseController {
-
   constructor() {
     super("")
     console.log('AuthHostController active');
     this.router
       .get('/login', this.authorizeHost)
       .get('/callback', this.authCallBack)
-
+      .use(auth0provider.getAuthorizedUserInfo)
+      .post('/tokensave', this.setHostTokens)
+      .get('/tokenget', this.getHostTokens)
   }
-
-
   async authorizeHost(req, res, next) {
     try {
       let html = await spotifyApi.createAuthorizeURL(scopes, '')
@@ -34,9 +29,7 @@ export class AuthHostController extends BaseController {
       // res.send({ url: html })
       res.redirect(html)
     } catch (error) { next(error) }
-
   }
-
   async authCallBack(req, res, next) {
     console.log("hit callback")
     const { code } = req.query;
@@ -45,7 +38,6 @@ export class AuthHostController extends BaseController {
       let data = await spotifyApi.authorizationCodeGrant(code)
       const { access_token, refresh_token, expires_in } = data.body;
       console.log(data.body);
-
       spotifyApi.setAccessToken(access_token);
       spotifyApi.setRefreshToken(refresh_token);
       let payload = {
@@ -53,18 +45,31 @@ export class AuthHostController extends BaseController {
         refreshToken: refresh_token,
         expiresIn: expires_in
       }
-
-      // authHostService.setHostTokens(payload)
       //unsafe.send(payload)
-
       res.redirect("http://localhost:8080/#/dashboard?" + `accessToken=${access_token}&refreshToken=${refresh_token}&expiresIn=${expires_in}`)
-
     } catch (error) {
       res.redirect('error')
     }
-
-
-
+  }
+  async setHostTokens(req, res, next) {
+    try {
+      req.body.creatorEmail = req.userInfo.email
+      let payload = {
+        creatorEmail: req.body.creatorEmail,
+        refreshToken: req.body.refreshToken,
+        accessToken: req.body.accessToken,
+        expiresIn: req.body.expiresIn
+      }
+      await authHostService.setHostTokens(payload)
+      res.send('Tokens saved')
+    } catch (error) {
+    }
+  }
+  async getHostTokens(req, res, next) {
+    try {
+      let result = await authHostService.getHostTokens(req.userInfo.email)
+      res.send(result)
+    } catch (error) {
+    }
   }
 }
-
