b40ef57e9adb0fb10dd8ce6908424aa4f4958def
tim hopp <timothyjhopp@gmail.com>
2020-09-17 15:43:12 -0600

    heroku changes

--- clientsrc/src/store/SocketStore.js
+++ clientsrc/src/store/SocketStore.js
@@ -5,7 +5,9 @@ let socket = {};
 export const socketStore = {
   actions: {
     initializeSocket({ commit, dispatch }) {
-      socket = io(window.location.host.includes('localhost') ? '//localhost:3000/' : '/')
+      socket = io(
+        window.location.host.includes("localhost") ? "//localhost:3000/" : "/"
+      );
       socket.on("CONNECTED", (data) => {
         console.log(data.message + " Let the villany commence");
       });
--- server/controllers/AuthHostController.js
+++ server/controllers/AuthHostController.js
@@ -1,41 +1,49 @@
-import express from 'express';
-import BaseController from '../utils/BaseController';
-import { authHostService } from '../services/AuthHostService.js'
+import express from "express";
+import BaseController from "../utils/BaseController";
+import { authHostService } from "../services/AuthHostService.js";
 import auth0provider from "@bcwdev/auth0provider";
-const SpotifyWebApi = require('spotify-web-api-node');
-let scopes = ['streaming', 'user-read-private', 'user-read-email', 'playlist-modify-public', 'playlist-modify-private']
+const SpotifyWebApi = require("spotify-web-api-node");
+let scopes = [
+  "streaming",
+  "user-read-private",
+  "user-read-email",
+  "playlist-modify-public",
+  "playlist-modify-private",
+];
 const spotifyApi = new SpotifyWebApi({
   clientId: process.env.SPOTIFY_CLIENT_ID,
   clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
-  redirectUri: 'http://localhost:3000/callback',
+  redirectUri: "https://songscoop.herokuapp.com/callback",
 });
-let unsafe = {}
+let unsafe = {};
 export class AuthHostController extends BaseController {
   constructor() {
-    super("")
+    super("");
     // console.log('AuthHostController active');
     this.router
-      .get('/callback', this.authCallBack)
-      .get('/login', this.authorizeHost)
-      .use('/auth', auth0provider.getAuthorizedUserInfo)
-      .post('/auth/tokensave', this.setHostTokens)
-      .get('/auth/tokenget', this.getHostTokens)
+      .get("/callback", this.authCallBack)
+      .get("/login", this.authorizeHost)
+      .use("/auth", auth0provider.getAuthorizedUserInfo)
+      .post("/auth/tokensave", this.setHostTokens)
+      .get("/auth/tokenget", this.getHostTokens);
   }
   async authorizeHost(req, res, next) {
     try {
-      let html = await spotifyApi.createAuthorizeURL(scopes, '')
+      let html = await spotifyApi.createAuthorizeURL(scopes, "");
       // let consoleHtml = new URL(html);
       console.log(html);
       // res.send({ url: html })
-      res.redirect(html)
-    } catch (error) { next(error) }
+      res.redirect(html);
+    } catch (error) {
+      next(error);
+    }
   }
   async authCallBack(req, res, next) {
-    console.log("hit callback")
+    console.log("hit callback");
     const { code } = req.query;
-    console.log(code)
+    console.log(code);
     try {
-      let data = await spotifyApi.authorizationCodeGrant(code)
+      let data = await spotifyApi.authorizationCodeGrant(code);
       const { access_token, refresh_token, expires_in } = data.body;
       // console.log(data.body);
       spotifyApi.setAccessToken(access_token);
@@ -43,33 +51,34 @@ export class AuthHostController extends BaseController {
       let payload = {
         accessToken: access_token,
         refreshToken: refresh_token,
-        expiresIn: expires_in
-      }
+        expiresIn: expires_in,
+      };
       //unsafe.send(payload)
-      res.redirect("https://songscoop.herokuapp.com/#/dashboard?" + `accessToken=${access_token}&refreshToken=${refresh_token}&expiresIn=${expires_in}`)
+      res.redirect(
+        "https://songscoop.herokuapp.com/#/dashboard?" +
+          `accessToken=${access_token}&refreshToken=${refresh_token}&expiresIn=${expires_in}`
+      );
     } catch (error) {
-      res.redirect('error')
+      res.redirect("error");
     }
   }
   async setHostTokens(req, res, next) {
     try {
-      req.body.creatorEmail = req.userInfo.email
+      req.body.creatorEmail = req.userInfo.email;
       let payload = {
         creatorEmail: req.body.creatorEmail,
         refreshToken: req.body.refreshToken,
         accessToken: req.body.accessToken,
-        expiresIn: req.body.expiresIn
-      }
-      await authHostService.setHostTokens(payload)
-      res.send('Tokens saved')
-    } catch (error) {
-    }
+        expiresIn: req.body.expiresIn,
+      };
+      await authHostService.setHostTokens(payload);
+      res.send("Tokens saved");
+    } catch (error) {}
   }
   async getHostTokens(req, res, next) {
     try {
-      let result = await authHostService.getHostTokens(req.userInfo.email)
-      res.send(result)
-    } catch (error) {
-    }
+      let result = await authHostService.getHostTokens(req.userInfo.email);
+      res.send(result);
+    } catch (error) {}
   }
 }
