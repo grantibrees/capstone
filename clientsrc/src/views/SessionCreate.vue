fcbbb76093e8631810fb32e5f5e5aa0898d65b49
MickShannahan <beasticorn@gmail.com>
2020-09-17 22:49:28 -0600

    join session and create session re-styled

--- clientsrc/src/views/SessionCreate.vue
+++ clientsrc/src/views/SessionCreate.vue
@@ -1,17 +1,8 @@
 <template>
-  <div class="SessionCreate container-fluid">
-    <div class="row chocolate min-height2 align-items-center justify-content-center">
-      <div class>
-        <div class="justify-content-center mt-2">
-          <h1 class="font-fancy">Song Sundae</h1>
-        </div>
-        <div class="justify-content-center mt-4 mx-5">
-          <p>You don't need to steal your friend's phone to put on decent music.</p>
-        </div>
-      </div>
-    </div>
+  <div class="SessionCreate full-height bg-warning container-fluid">
+    <div class="row chocolate align-items-center justify-content-center"></div>
 
-    <div class="row vanilla min-height align-items-center">
+    <div class="row vanilla p-push align-items-center">
       <div class="col-12">
         <div class="form-group">
           <div class="row px-5 py-3">
@@ -22,20 +13,14 @@
               class="form-control text-center rounded-pill"
             />
           </div>
-          <div class="row px-5 py-3">
-            <button
-              @click="createSession"
-              class="btn btn-block btn-outline-danger p-3 rounded-pill"
-            >
+          <div class="border-pill-wrap">
+            <button @click="createSession" class="btn btn-block btn-pill p-3 rounded-pill">
               <h2>Create Session</h2>
             </button>
           </div>
-
         </div>
       </div>
     </div>
-    <div class="row min-height2 strawberry"></div>
-  </div>
 </template>
 
 
@@ -75,36 +60,18 @@ export default {
 
 
 <style scoped>
-body {
-  background-color: #fff8ed;
-  color: var(--black);
-}
-.font-fancy {
-  font-family: "Norican", cursive;
+.full-height {
+  min-height: 100vh;
 }
 
-.min-height {
-  min-height: 36vh;
+.btn-pill {
+  background-color: var(--warning);
 }
-.min-height2 {
-  min-height: 32vh;
+.btn-pill:hover {
+  color: var(--info);
 }
 
-.strawberry {
-  background-color: #ffd9d1;
-}
-.chocolate {
-  background-color: #74462c;
-}
-.vanilla {
-  background-color: #fff8ed;
-}
-.strawberry-accent {
-  color: white;
-  background-color: #e64772;
-}
-.bright-accent {
-  color: white;
-  background-color: #0fb2b5;
+.p-push {
+  padding-top: 12em;
 }
 </style>
--- clientsrc/utils.js
+++ clientsrc/utils.js
@@ -1,7 +1,7 @@
 export default {
   randomCode() {
     let genderatedCode = []
-    let code = "ABCDEFGHJIKLMNPQRSTUVWXYZ1234567890"
+    let code = "ABCDEFGHJKLMNPQRSTUVWXYZ23456789"
     for (let i = 1; i <= 4; i++) {
       let index = Math.floor(Math.random() * code.length)
       genderatedCode.push(code[index])
